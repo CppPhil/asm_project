11782f1794795747b5aa0d5469693e7b076ea5da
Phil <CppPhil@users.noreply.github.com>
2018-02-22 06:15:17 +0100

    documentation

--- include/memcpy_asm.h
+++ include/memcpy_asm.h
@@ -2,6 +2,19 @@
 #define INCG_AP_MEMCPY_ASM_H
 #include <stdint.h> // uint64_t
 
+/*!
+ * \brief Copies count characters from the object pointed to by 'src' to 
+ *        the object pointed to by 'dest'. Both objects are interpreted as 
+ *        arrays of unsigned char.
+ * \param dest pointer to the object to copy to 
+ * \param src pointer to the object to copy from 
+ * \param count number of bytes to copy
+ * \return Returns a copy of dest
+ * \warning The behavior is undefined if access occurs beyond the end 
+ *          of the 'dest' array. If the objects overlap (which is a violation
+ *          of the restrict contract), the behavior is undefined. 
+ *          The behavior is undefined if either 'dest' or 'src' is a null pointer.
+**/
 void *memcpy_asm(
     void * restrict dest, 
     const void * restrict src, 
